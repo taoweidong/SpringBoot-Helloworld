38fb17104df09a8bd91dcdd3c6a49cd95bba9d73
Taowd <taowd@outlook.com>
2017-08-12 17:52:09 +0800

    过滤之后的项目文件

---
+++ src/main/java/com/App.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	/**
+	 * 功能：Spring-boot的启动程序(入口)
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+}
